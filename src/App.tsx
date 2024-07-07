9151067780131a8a07dc299eb06c9f389cb88e39
alisherova <alisherovanurjahon@gmail.com>
2024-07-08 01:13:27 +0200

    feat: complete the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-function App() {
+import { Component } from 'react';
+import ErrorBoundary from '../components/ErrorBoundary';
+import SearchSection from '../components/SearchSection';
+import ResultsSection from '../components/ResultsSection';
 
-  return (
-    <>
-    </>
-  );
+
+class App extends Component {
+  state = {
+    searchTerm: localStorage.getItem('searchTerm') ? localStorage.getItem('searchTerm') : " ",
+  };
+
+  handleSearchTermChange = (newSearchTerm: string) => {
+    this.setState({ searchTerm: newSearchTerm.trim() });
+  };
+
+  render() {
+    const { searchTerm } = this.state;
+    return (
+      <ErrorBoundary>
+        <div className="app">
+          <SearchSection onSearch={this.handleSearchTermChange} />
+          <ResultsSection searchTerm={searchTerm} />
+        </div>
+      </ErrorBoundary>
+    );
+  }
 }
 
 export default App;
